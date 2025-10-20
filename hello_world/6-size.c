79fca1a7c64b1293ce117b0852ba4d093941a2f5
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-20 15:58:27 +0000

    8

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,6 +7,10 @@
 */
 int main(void)
 {
-	puts("size32");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 8 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
 	return (0);
 }
