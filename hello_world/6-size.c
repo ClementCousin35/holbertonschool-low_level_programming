f968b175350a6212b79ef158c9af2e05c1d64bc0
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-20 16:30:22 +0000

    8

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,21 +5,12 @@
 *
 * Return: Always 0 (Success)
 */
-int size32(void)
+int main(void)
 {
 	printf("Size of a char: 1 byte(s)\n");
         printf("Size of an int: 4 byte(s)\n");
         printf("Size of a long int: 4 byte(s)\n");
         printf("Size of a long long int: 8 byte(s)\n");
         printf("Size of a float: 4 byte(s)\n");
-	return (0);
-}
-int size64(void)
-{
-	printf("Size of a char: 1 byte(s)\n");
-	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of a long int: 8 byte(s)\n");
-	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)\n");
 	return (0);
 }
