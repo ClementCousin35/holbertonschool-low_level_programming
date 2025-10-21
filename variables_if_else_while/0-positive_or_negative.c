07d129321c312c43bc17999fcbe24d0d44e5800a
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-21 08:18:07 +0000

    8

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if(n > 0);
-		printf(is positive\n);
+		printf("is positive\n");
 	if(n = 0);
-		printf(is zero\n);
+		printf("is zero\n");
 	if(n < 0);
-		printf(is negative\n);
+		printf("is negative\n");
 	return (0);
 }
