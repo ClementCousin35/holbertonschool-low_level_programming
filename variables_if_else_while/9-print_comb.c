5939d3a82d523539a19a43a0be0e0facee06694c
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-22 09:17:51 +0000

    8

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
 * main - Entry point
@@ -9,11 +9,17 @@ int main(void)
 {
 	int c;
 
-	for (c = '0'; c <= '8'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 	putchar(c);
+	if (c == '9')
+	{
+	}
+	else
+	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
