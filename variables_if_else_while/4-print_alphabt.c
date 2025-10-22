f99af71e1c22f0218c56c3e21ee80831577756b3
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-22 07:19:50 +0000

    8

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+        putchar(c);
+	}
+	if (c = 'q'; c = 'e')
+	{
+	remove("c");
+        }
+        putchar('\n');
+        return (0);
+}
