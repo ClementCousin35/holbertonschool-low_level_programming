bc6b7c9055b401bad56027cdfc02c122fe8acc6f
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-23 14:37:48 +0000

    8

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* main - Entry point
+* void print_alphabet(void)
 *
 * Return: Always 0 (Success)
 */
-int main (void)
+void print_alphabet(void)
 {
+
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
@@ -14,5 +15,4 @@ int main (void)
 	_putchar(c);
 	}
 	_putchar('\n');
-	return 0;
 }
