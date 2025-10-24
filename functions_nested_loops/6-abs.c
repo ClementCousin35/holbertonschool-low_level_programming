faa26a8a104cf62cb39313105a3f2d4e8dbae53e
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-24 13:05:28 +0000

    8

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs- aide pour la conduite pour le systeme de frein
+* @a: le a initie les chiffres
+* Return: Always 0 (Success)
+*/
+
+int _abs(int a)
+{
+	if (a > 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (0);
+	}
+}
