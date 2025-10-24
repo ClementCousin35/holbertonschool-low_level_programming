156bb17f489f13e0b9e6a7342ddf9fce0d11714d
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-24 09:49:51 +0000

    8

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower- Je sais pas ce qu'elle fais merci
+* description pour le c- tu es bien chiant
+* Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
