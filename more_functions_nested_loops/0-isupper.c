95116cbd59f7ac1158e23ba46f5d8d2c2f7ab013
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-27 08:13:51 +0000

    8

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isupper- il fais des trucs plutot pas mal
+* @c: int le c pour qu'il vaux un truc
+* Return: Always 0 (Success)
+*/
+
+int _isupper(int c)
+{
+	if(c= 'A', c= 'Z')
+	{
+	return (1);
+	}
+	else(c= 'a', c= 'z')
+	{
+	return (0);
+	}
+}
