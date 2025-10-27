ccdb828510395d28b2a3fae86cb537ef9e0f53d1
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-27 08:56:42 +0000

    8

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers- Starfoula je te alala betty
+* @c: Salut c moi c=c'est :)
+* Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
