213bac2e147656a545c023aa478e0df17367b311
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-27 14:04:03 +0000

    8

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_diagonal- ecris des lignes pour chaque unite
+* @n: tres compliquer la task
+* Return: Always 0 (Success)
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+		_putchar('\\');
+		}
+	_putchar('\n');
+	}
+}
