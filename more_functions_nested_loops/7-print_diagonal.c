f951278f1df26aad6e3f712f14d8704037947a0c
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-27 18:04:27 +0000

    8

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,18 @@
 
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int i, j;
 
 	if (n <= 0)
 	{
 	_putchar('\n');
 	}
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-		_putchar(' ');
-			for (j = i + 1; j < i; j++)
-			{
-			_putchar(' ');
-			}
-		}
+	for (j = 0; j < i; j++)
+	{
+	_putchar(' ');
+	}
 	_putchar('\\');
 	_putchar('\n');
 	}
