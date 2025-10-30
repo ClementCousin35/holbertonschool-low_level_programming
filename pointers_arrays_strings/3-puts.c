e8e0e25d3a3b8ee3ca5815df350f0d130822bf43
ClementCousin35 <clement.cousin-35@hotmail.com>
2025-10-30 11:06:50 +0000

    8

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts- affiche les messages
+* @str: message a transcrire
+* Return: Always 0 (Success)
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
